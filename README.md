0da2e50561784aa1286cc32ff8a7178a49e49a63
Zhang Johnnie <yixizhang@ebay.com>
2016-01-14 21:10:00 -0800

    change readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # travis-example
 travis-example
+
+[![Build Status](https://travis-ci.org/joohnnie/travis-example.svg?branch=master)](https://travis-ci.org/joohnnie/travis-example)
---
+++ src/main/scala/org/dooit/example/HelloWorld.scala
@@ -0,0 +1,15 @@
+package org.dooit.example
+
+/**
+  * Created  01/14/2016 | 21:06
+  *
+  * @author : Johnnie Zhang
+  * @version : 1.0.0
+  * @since : 1.0.0
+  *
+  */
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("hello world")
+  }
+}
